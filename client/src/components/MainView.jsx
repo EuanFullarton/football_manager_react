e11a318127f723b855c0fda6aaac881efbfcb538
Euan Fullarton <euanfullarton7@gmail.com>
2017-06-15 09:56:11 +0100

    Removed sendHTTPRequest function for now, not being used

--- client/src/components/MainView.jsx
+++ client/src/components/MainView.jsx
@@ -4,22 +4,6 @@ class MainView extends React.Component {
 
   constructor(props) {
     super(props);
-    this.sendHTTPRequest = this.sendHTTPRequest.bind(this);
-    this.state = {
-      teams: []
-    }
-  }
-
-  sendHTTPRequest(url) {
-    const request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.onload = () => {
-      if(request.status === 200){
-        const teams = JSON.parse(request.responseText);
-        this.setState({teams: teams});
-      }
-    };
-    request.send(null);
   }
 
   render(){
